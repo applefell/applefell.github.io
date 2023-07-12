185adc0ab9ba6e11d0e8028bdd92927ffc863aab
Apple.Fell <aflyingpeiceofglue@gmail.com>
2023-07-11 17:28:19 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// set width of sidenav to 250px
+function openNav() {
+	document.getElementById("mySidenav").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+}
+
+// set width of sidenav to 0
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("main").style.marginLeft = "0";
+}
